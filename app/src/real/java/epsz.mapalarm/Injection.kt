9530531de3a8723347d82e811f65a509636e56c1
Martin Epszteyn <mepszteyn@gmail.com>
2015-12-05 13:42:08 -0300

    Fiddled with gradle settings for building

---
+++ app/src/real/java/epsz.mapalarm/Injection.kt
@@ -0,0 +1,12 @@
+package epsz.mapalarm
+
+import com.mapalarm.Environment
+import com.mapalarm.TriggersGateway
+import com.mapalarm.datatypes.Position
+import com.mapalarm.entities.PositionTrigger
+
+class Injection {
+    init {
+
+    }
+}
